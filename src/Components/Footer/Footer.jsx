c1508101e0deab5334b11c695217721afe8d1cde
Gabriel Reis <gabrielreistech@gmail.com>
2025-02-10 12:34:03 -0300

    Criação do Footer e alguns ajustes no Header

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+    return(
+      <div className={styles.container}>
+        <h2>Por enquanto apenas uma frase para preencher o espaço</h2>
+      </div> 
+    );
+}
+
+export default Footer;
